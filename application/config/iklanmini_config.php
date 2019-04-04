851a0792754fd7331012e834b7d30b7504a9ddea
Dwi Wahyudi <dwi_wahyudi@yahoo.com>
2019-04-04 23:55:12 +0700

    Update Core Code ADD THEMES FOLDER ADD PUBLIC FOLDER SETTING CONFIG

---
+++ application/config/iklanmini_config.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$config['themes']       = 'iklanmini';
+$config['web_title']    = 'Iklan Mini ::';
+$config['meta_desc']    = 'Iklan Mini ::';
+$config['meta_keywords']= 'Iklan Mini ::';
+
---
+++ application/models/Mdl_utama.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mdl_utama extends CI_Model {
+    
+    
+}
